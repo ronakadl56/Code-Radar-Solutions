08e63253175fb9419b2ab4c3028437831724b7b9
Rhythm <rhythmadhalakha@gmail.com>
2025-02-18 10:56:20 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,8 @@ int main()
 {
     int a, b;
     char op;
-    scanf("%d%d%c", &a, &b, &op);
+    scanf("%d%d", &a, &b);
+    scanf("%c", &op);
     if(op=="+")
     printf("%d", a+b);
     else if(op=="-")
