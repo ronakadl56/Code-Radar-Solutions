c9ac4a0a6763f0009bfdab08f4f1eb728e4f2928
Rhythm <rhythmadhalakha@gmail.com>
2025-03-12 23:15:55 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -3,20 +3,24 @@
 int findMinLengthSubarray(int arr[], int n) {
     int start = 0, end = n - 1;
 
+    // Find the first element which is out of order from the beginning
     while (start < n - 1 && arr[start] <= arr[start + 1]) {
         start++;
     }
 
+    // If the array is already sorted
     if (start == n - 1) {
         return 0;
     }
 
+    // Find the first element which is out of order from the end
     while (end > 0 && arr[end] >= arr[end - 1]) {
         end--;
     }
 
     int minSubarrayLen = end - start + 1;
 
+    // Find the minimum and maximum values in the subarray
     int minVal = arr[start], maxVal = arr[start];
     for (int i = start; i <= end; i++) {
         if (arr[i] < minVal) {
@@ -39,13 +43,16 @@ int findMinLengthSubarray(int arr[], int n) {
 
 int main() {
     int T;
+
     scanf("%d", &T);
 
     while (T--) {
         int N;
+
         scanf("%d", &N);
 
         int arr[N];
+
         for (int i = 0; i < N; i++) {
             scanf("%d", &arr[i]);
         }
