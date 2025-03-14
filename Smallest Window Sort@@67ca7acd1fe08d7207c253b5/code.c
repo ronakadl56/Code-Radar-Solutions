88979dea08753bc4b637438e32ca61823bf9b8fe
Rhythm <rhythmadhalakha@gmail.com>
2025-03-14 23:51:43 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,65 +1,37 @@
-#include <stdio.h>
+int findUnsortedSubarray(int arr[], int n) {
+    int left = 0, right = n - 1;
 
-int findMinLengthSubarray(int arr[], int n) {
-    int start = 0, end = n - 1;
-
-    // Find the first element which is out of order from the beginning
-    while (start < n - 1 && arr[start] <= arr[start + 1]) {
-        start++;
+    // Find the first element which is out of order from the left
+    while (left < n - 1 && arr[left] <= arr[left + 1]) {
+        left++;
     }
 
-    // If the array is already sorted
-    if (start == n - 1) {
+    // If the entire array is already sorted
+    if (left == n - 1) {
         return 0;
     }
 
-    // Find the first element which is out of order from the end
-    while (end > 0 && arr[end] >= arr[end - 1]) {
-        end--;
+    // Find the first element which is out of order from the right
+    while (right > 0 && arr[right] >= arr[right - 1]) {
+        right--;
     }
 
-    int minSubarrayLen = end - start + 1;
-
-    // Find the minimum and maximum values in the subarray
-    int minVal = arr[start], maxVal = arr[start];
-    for (int i = start; i <= end; i++) {
-        if (arr[i] < minVal) {
-            minVal = arr[i];
-        }
-        if (arr[i] > maxVal) {
-            maxVal = arr[i];
-        }
+    // Find the min and max within the unsorted subarray
+    int minVal = arr[left], maxVal = arr[left];
+    for (int i = left; i <= right; i++) {
+        if (arr[i] < minVal) minVal = arr[i];
+        if (arr[i] > maxVal) maxVal = arr[i];
     }
 
-    while (start > 0 && arr[start - 1] > minVal) {
-        start--;
-    }
-    while (end < n - 1 && arr[end + 1] < maxVal) {
-        end++;
+    // Expand left boundary if needed
+    while (left > 0 && arr[left - 1] > minVal) {
+        left--;
     }
 
-    return end - start + 1;
-}
-
-int main() {
-    int T;
-
-    scanf("%d", &T);
-
-    while (T--) {
-        int N;
-
-        scanf("%d", &N);
-
-        int arr[N];
-
-        for (int i = 0; i < N; i++) {
-            scanf("%d", &arr[i]);
-        }
-
-        int result = findMinLengthSubarray(arr, N);
-        printf("%d\n", result);
+    // Expand right boundary if needed
+    while (right < n - 1 && arr[right + 1] < maxVal) {
+        right++;
     }
 
-    return 0;
+    return right - left + 1;
 }
