ca7617fb064eadbe80d31297b2cf5fa353b17278
Rhythm <rhythmadhalakha@gmail.com>
2025-03-28 16:30:23 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,47 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-
-// Function to sort an array of strings using Bubble Sort
+#include<stdio.h>
+#include<string.h>
 void bubbleSort(char arr[][100], int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            // Compare adjacent strings and swap if they are in the wrong order
-            if (strcmp(arr[j], arr[j + 1]) > 0) {
-                char temp[100];
-                strcpy(temp, arr[j]);
-                strcpy(arr[j], arr[j + 1]);
-                strcpy(arr[j + 1], temp);
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (strcmp(arr[j], arr[minIndex]) < 0) {
+                minIndex = j;
             }
+        }
+
+        if (minIndex != i) {
+            char temp[100];
+            strcpy(temp, arr[i]);
+            strcpy(arr[i], arr[minIndex]);
+            strcpy(arr[minIndex], temp);
         }
     }
 }
-
-// Function to print an array of strings
 void printArray(char arr[][100], int n) {
     for (int i = 0; i < n; i++) {
         printf("%s\n", arr[i]);
     }
-}
-
-// Main function where program execution starts
-int main() {
-    int n; // Variable to store the number of strings
-
-    // Read the number of strings from user input
-    scanf("%d", &n);
-
-    char arr[n][100]; // Declare an array of strings with max length 100
-
-    // Read the strings from user input
-    for (int i = 0; i < n; i++) {
-        scanf("%s", arr[i]);
-    }
-
-    // Call the bubbleSort function
-    bubbleSort(arr, n);
-
-    // Display the sorted array of strings
-    printArray(arr, n);
-
-    return 0; // Indicate successful program termination
 }
